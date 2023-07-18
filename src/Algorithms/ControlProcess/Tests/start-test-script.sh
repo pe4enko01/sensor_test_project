a489eb644ab99ac734ace2d5f4f7d0d74f4d500c
pe4enko01 <pe4enko02@yandex.ru>
2023-07-18 15:21:47 +0300

    Add test-script

---
+++ src/Algorithms/ControlProcess/Tests/start-test-script.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+START=uniset2-start.sh
+pushd ../../../Services/Administrator/
+./create
+popd
+sleep 1 
+pushd ../../../SharedMemory/
+screen sh start_share.sh
+popd
+sleep 2
+pushd ../../../../Build/src/Algorithms/ControlProcess/
+pwd
+screen sh start_fg.sh
+popd
+sleep 1
+
+$START  -f uniset2-testsuite-xmlplayer --log-show-tests --confile ../configure.xml --testfile test-control-process.xml --show-result-report --show-test-log $*
